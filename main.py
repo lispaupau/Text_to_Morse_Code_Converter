3a65b12c2a564cec7ad65f197b3cb949d554e593
lispaupau <152isu@mail.ru>
2024-03-22 19:57:59 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+morse_code = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+    '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----', ' ': ' '
+}
+while True:
+    try:
+        user_message = input('Enter the message to convert to Morse or "q" to exit: ')
+        if user_message.upper() == 'Q':
+            break
+
+        encoded_message = ' '.join([morse_code[char] for char in user_message.upper()])
+        print(encoded_message)
+    except KeyError:
+        print('Use Latin alphabet and numbers')
